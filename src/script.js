2bcc840849ca9c2f4c65e8f386a4dec74621c8f2
Taya Hanchar <tayahanchar@gmail.com>
2023-10-18 23:44:37 +0300

    feat: add video journey section

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
 import './scss/style.scss';
+
+const progress = document.querySelector('.progress');
+  
+progress.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+});
+
+const progressValume = document.querySelector('.progress-valume');
+  
+progressValume.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+});
